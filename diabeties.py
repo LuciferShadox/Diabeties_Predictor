a4bf7b2e99799f500e0ab9325fd32aec3513cbd4
Rahul Ramakrishnan <40205748+LuciferShadox@users.noreply.github.com>
2018-11-09 18:42:52 +0530

    Add files via upload

---
+++ diabeties.py
@@ -0,0 +1,40 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+#importing library finctions
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split #split dataset into test and train cases
+from sklearn.naive_bayes import GaussianNB # import library of NB
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+def get_data(filename):
+    dataset=pd.read_csv(filename)
+    x=dataset.iloc[:,0:8]#select all attribute columns
+    y=dataset.iloc[:,8]#select outputs
+    return x,y
+
+
+# train data
+x,y=get_data('diabetes.csv')
+x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=0, test_size=0.20)
+classifier=GaussianNB()#using naivce bayes model
+classifier.fit(x_train,y_train)
+y_pred=classifier.predict(x_test) #predict output
+
+
+
+cm=confusion_matrix(y_test,y_pred)#comparing with test cases
+print cm
+
+print(accuracy_score(y_test,y_pred))
+plt.plot(y_test,y_pred,color='red', label="NB")
+
+
+
+
+    
+        
+
+# Any results you write to the current directory are saved as output.
